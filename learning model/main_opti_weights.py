e379f4d07fb3e8da697414bd30b5db769ac860f9
StijnStaring <stijnstaring@hotmail.com>
2020-03-23 22:50:33 +0100

    23-03-22-50

--- learning model/main_opti_weights.py
+++ learning model/main_opti_weights.py
@@ -46,7 +46,10 @@ update = del_0*plt.ones([amount_features,1])
 #################
 # Comfort cost function: ax**2+t1*ay**2+t2*jy**2+t3*(vx-vdes)**2+t4*(y-ydes)**2
 # theta = plt.array([[1.5],[1.5],[1.5],[1.5],[1.5]])
-theta = plt.array([[1.0],[1.0],[1.0],[1.0],[1.0]])
+# theta = plt.array([[1.0],[1.0],[1.0],[1.0],[1.0]])
+# theta = plt.array([[5.49749001e+02], [1.89525204e+00], [5.31749963e-01], [2.14306117e+01],[1.16706616e-01]])
+theta = plt.array([[551], [1.93], [0.55], [23], [0.15]])
+
 theta_tracker = []
 theta_tracker.append(theta)
 # theta = 1.5*plt.ones((amount_features,1))
@@ -69,15 +72,15 @@ for file in file_list:
 
     # Optimization loop
     # Change this to convergence criterium: df/dt = 0 or weights are accurately found.
-    # theta_chosen = plt.array([5.49749001e+02, 1.89525204e+00, 5.31749963e-01, 2.14306117e+01, 1.16706616e-01])
-    theta_chosen = plt.array([4,5,6,1,2])
+    theta_chosen = plt.array([5.49749001e+02, 1.89525204e+00, 5.31749963e-01, 2.14306117e+01, 1.16706616e-01])
+
+    # theta_chosen = plt.array([4,5,6,1,2])
     theta_chosen = theta_chosen[:,plt.newaxis]
     f_calc_rel = plt.array([[100],[100],[100],[100],[100]]) # just start value
     # while plt.sum(plt.absolute(grad_curr)) < 0.5:
     # while plt.sum(plt.absolute(theta_chosen - theta)) > 0.5:
 
     while any(plt.absolute(f_calc_rel-1) >= 1e-4):
-    # while rec <3 :
         print('rec is: ',rec)
         plotting_calc = 0
         [data_s, f_calc] = optim_weights_ideal(theta,rec,N,plotting_calc,axcom1a,axcom1b,axcom2,axcom3a,axcom3b,axcom4a,axcom4b,axcom5a,axcom5b,axcom6a,axcom6b,axcom7a,axcom7b,axcom8a,axcom8b,file)
@@ -93,20 +96,20 @@ for file in file_list:
         print('This is the difference of theta: ', theta_chosen - theta)
         his_diff_theta.append([str(rec) + "//", theta_chosen - theta])
         print('This is grad current: ', plt.sum(plt.absolute(grad_curr)))
-        [grad_prev, del_theta_prev, exception, theta, update, multi_grads] = RPROP(grad_curr,grad_prev,update,theta,del_theta_prev,exception)
-        his_multi_grads.append([str(rec)+"//",multi_grads])
+        axf.plot([f_calc_rel[0], f_calc_rel[1], f_calc_rel[2], f_calc_rel[3], f_calc_rel[4]], '-', marker='o', markersize=6,label="Calc Features iter: " + str(rec))
+        axf.legend()
+        axfn.plot([f_calc[0], f_calc[1], f_calc[2], f_calc[3], f_calc[4]], '-', marker='o', markersize=6,label="Calc Features iter: " + str(rec))
+        axfn.legend()
         his_grad_current.append([str(rec) + "//", grad_curr])
 
-        # Plots in the for loop
-        axf.plot([f_calc_rel[0], f_calc_rel[1], f_calc_rel[2], f_calc_rel[3], f_calc_rel[4]],'-', marker='o', markersize=6, label="Calc Features iter: " + str(rec))
-        axf.legend()
-        axfn.plot([f_calc[0],f_calc[1],f_calc[2],f_calc[3],f_calc[4]],'-', marker='o', markersize=6, label="Calc Features iter: " + str(rec))
-        axfn.legend()
-        rec = rec + 1
+        if any(plt.absolute(f_calc_rel - 1) >= 1e-4):
+            [grad_prev, del_theta_prev, exception, theta, update, multi_grads] = RPROP(grad_curr,grad_prev,update,theta,del_theta_prev,exception)
+            his_multi_grads.append([str(rec)+"//",multi_grads])
+            rec = rec + 1
+            his_weights.append([str(rec) + "//", theta])
+            acw.plot([theta[0], theta[1], theta[2], theta[3], theta[4]], '-', marker='o', markersize=6,label="iter " + str(rec))
+            acw.legend()
 
-        his_weights.append([str(rec) + "//", theta])
-        acw.plot([theta[0], theta[1], theta[2], theta[3], theta[4]], '-', marker='o', markersize=6,label="iter " + str(rec))
-        acw.legend()
         ###########################
 
     # Post - processing
@@ -139,63 +142,64 @@ for file in file_list:
     print("This is the history of the update of the weights.")
     print("----------------------------------------------------")
     print('\n')
-    for i in plt.arange(1,len(his_weights),1):
-        print("This is update " + str(i))
-        print(his_weights[i][1] - his_weights[i-1][1])
+    if len(his_weights) != 1:
+        for i in plt.arange(1,len(his_weights),1):
+            print("This is update " + str(i))
+            print(his_weights[i][1] - his_weights[i-1][1])
 
     post_processing_plots(his_f_calc_rel,his_weights,his_multi_grads,his_grad_current,his_diff_theta)
 
     # # Plotting end solution in comparinson
-    # data_s = dict_sol_list[-1]
-    # x_sol = data_s['x_s']
-    # y_sol = data_s['y_s']
-    # vx_sol = data_s['vx_s']
-    # vy_sol = data_s['vy_s']
-    # psi_sol = data_s['psi_s']
-    # psi_dot_sol = data_s['psi_dot_s']
-    # throttle_sol = data_s['throttle_s']
-    # delta_sol = data_s['delta_s']
-    # T_sol = data_s['T_s']
-    # dt_sol = data_s['dt_s']
-    # ax_tot_sol = data_s['ax_tot_s']
-    # ay_tot_sol = data_s['ay_tot_s']
-    # aty_sol = data_s['aty_s']
-    # any_sol = data_s['any_s']
-    # jx_sol = data_s['jx_s']
-    # jy_sol = data_s['jy_s']
-    #
-    # time_vector = plt.linspace(0, T_sol, len(x_sol))
-    # axcom1a.plot(time_vector, x_sol, '.-', linewidth=3.0, label="learned solution")
-    # axcom1b.plot(time_vector, y_sol, '.-', linewidth=3.0, label="learned solution")
-    # axcom2.plot(x_sol, y_sol, '.-', linewidth=3.0, label="learned solution")
-    # axcom3a.plot(time_vector, vx_sol, '.-', linewidth=3.0, label="learned solution")
-    # axcom3b.plot(time_vector, vy_sol, '.-', linewidth=3.0, label="learned solution")
-    # axcom4a.plot(time_vector, ax_tot_sol, '.-', linewidth=3.0, label="learned solution")
-    # axcom4b.plot(time_vector, ay_tot_sol, '.-', linewidth=3.0, label="learned solution")
-    # axcom5a.plot(time_vector, jx_sol, '.-', linewidth=3.0, label="learned solution")
-    # axcom5b.plot(time_vector, jy_sol, '.-', linewidth=3.0, label="learned solution")
-    # axcom6a.plot(time_vector, psi_sol * 180 / plt.pi, '.-', linewidth=3.0, label="learned solution")
-    # axcom6b.plot(time_vector, psi_dot_sol * 180 / plt.pi, '.-', linewidth=3.0, label="learned solution")
-    # axcom7a.plot(time_vector[0:-1], throttle_sol, '.-', linewidth=3.0, label="learned solution")
-    # axcom7b.plot(time_vector[0:-1], delta_sol * 180 / plt.pi, '.-', linewidth=3.0, label="learned solution")
-    # axcom8a.plot(time_vector, aty_sol, '.-', linewidth=3.0, label="learned solution")
-    # axcom8b.plot(time_vector, any_sol, '.-', linewidth=3.0, label="learned solution")
-    #
-    # axcom1a.legend()
-    # axcom1b.legend()
-    # axcom2.legend()
-    # axcom3a.legend()
-    # axcom3b.legend()
-    # axcom4a.legend()
-    # axcom4b.legend()
-    # axcom5a.legend()
-    # axcom5b.legend()
-    # axcom6a.legend()
-    # axcom6b.legend()
-    # axcom7a.legend()
-    # axcom7b.legend()
-    # axcom8a.legend()
-    # axcom8b.legend()
+    data_s = dict_sol_list[-1]
+    x_sol = data_s['x_s']
+    y_sol = data_s['y_s']
+    vx_sol = data_s['vx_s']
+    vy_sol = data_s['vy_s']
+    psi_sol = data_s['psi_s']
+    psi_dot_sol = data_s['psi_dot_s']
+    throttle_sol = data_s['throttle_s']
+    delta_sol = data_s['delta_s']
+    T_sol = data_s['T_s']
+    dt_sol = data_s['dt_s']
+    ax_tot_sol = data_s['ax_tot_s']
+    ay_tot_sol = data_s['ay_tot_s']
+    aty_sol = data_s['aty_s']
+    any_sol = data_s['any_s']
+    jx_sol = data_s['jx_s']
+    jy_sol = data_s['jy_s']
+
+    time_vector = plt.linspace(0, T_sol, len(x_sol))
+    axcom1a.plot(time_vector, x_sol, '.-', linewidth=3.0, label="learned solution")
+    axcom1b.plot(time_vector, y_sol, '.-', linewidth=3.0, label="learned solution")
+    axcom2.plot(x_sol, y_sol, '.-', linewidth=3.0, label="learned solution")
+    axcom3a.plot(time_vector, vx_sol, '.-', linewidth=3.0, label="learned solution")
+    axcom3b.plot(time_vector, vy_sol, '.-', linewidth=3.0, label="learned solution")
+    axcom4a.plot(time_vector, ax_tot_sol, '.-', linewidth=3.0, label="learned solution")
+    axcom4b.plot(time_vector, ay_tot_sol, '.-', linewidth=3.0, label="learned solution")
+    axcom5a.plot(time_vector, jx_sol, '.-', linewidth=3.0, label="learned solution")
+    axcom5b.plot(time_vector, jy_sol, '.-', linewidth=3.0, label="learned solution")
+    axcom6a.plot(time_vector, psi_sol * 180 / plt.pi, '.-', linewidth=3.0, label="learned solution")
+    axcom6b.plot(time_vector, psi_dot_sol * 180 / plt.pi, '.-', linewidth=3.0, label="learned solution")
+    axcom7a.plot(time_vector[0:-1], throttle_sol, '.-', linewidth=3.0, label="learned solution")
+    axcom7b.plot(time_vector[0:-1], delta_sol * 180 / plt.pi, '.-', linewidth=3.0, label="learned solution")
+    axcom8a.plot(time_vector, aty_sol, '.-', linewidth=3.0, label="learned solution")
+    axcom8b.plot(time_vector, any_sol, '.-', linewidth=3.0, label="learned solution")
+
+    axcom1a.legend()
+    axcom1b.legend()
+    axcom2.legend()
+    axcom3a.legend()
+    axcom3b.legend()
+    axcom4a.legend()
+    axcom4b.legend()
+    axcom5a.legend()
+    axcom5b.legend()
+    axcom6a.legend()
+    axcom6b.legend()
+    axcom7a.legend()
+    axcom7b.legend()
+    axcom8a.legend()
+    axcom8b.legend()
 print('This is the theta_tracker: ',theta_tracker)
 plt.show()
 #####################
--- learning model/post_processing_plots.py
+++ learning model/post_processing_plots.py
@@ -185,15 +185,15 @@ def post_processing_plots(his_f_calc_rel,his_weights,his_multi_grads,his_grad_cu
     # plt.ylabel("delta weight value [-]", fontsize=14)
     # plt.grid(True)
     # plt.title('Update of theta 7', fontsize=14)
-
-    for i in plt.arange(1, length, 1):
-        ac1.plot(iterations[i], his_weights[i][1][0]-his_weights[i-1][1][0], 'o', label="iteration " + str(i + 1), linewidth=3.0)
-        ac2.plot(iterations[i], his_weights[i][1][1]-his_weights[i-1][1][1], 'o', label="iteration " + str(i + 1), linewidth=3.0)
-        ac3.plot(iterations[i], his_weights[i][1][2]-his_weights[i-1][1][2], 'o', label="iteration " + str(i + 1), linewidth=3.0)
-        ac4.plot(iterations[i], his_weights[i][1][3]-his_weights[i-1][1][3], 'o', label="iteration " + str(i + 1), linewidth=3.0)
-        ac5.plot(iterations[i], his_weights[i][1][4]-his_weights[i-1][1][4], 'o', label="iteration " + str(i + 1), linewidth=3.0)
-        # ac6.plot(iterations[i], his_weights[i][1][5]-his_weights[i-1][1][5], 'o', label="iteration " + str(i + 1), linewidth=3.0)
-        # ac7.plot(iterations[i], his_weights[i][1][6]-his_weights[i-1][1][6], 'o', label="iteration " + str(i + 1), linewidth=3.0)
+    if length != 1:
+        for i in plt.arange(1, length, 1):
+            ac1.plot(iterations[i], his_weights[i][1][0]-his_weights[i-1][1][0], 'o', label="iteration " + str(i + 1), linewidth=3.0)
+            ac2.plot(iterations[i], his_weights[i][1][1]-his_weights[i-1][1][1], 'o', label="iteration " + str(i + 1), linewidth=3.0)
+            ac3.plot(iterations[i], his_weights[i][1][2]-his_weights[i-1][1][2], 'o', label="iteration " + str(i + 1), linewidth=3.0)
+            ac4.plot(iterations[i], his_weights[i][1][3]-his_weights[i-1][1][3], 'o', label="iteration " + str(i + 1), linewidth=3.0)
+            ac5.plot(iterations[i], his_weights[i][1][4]-his_weights[i-1][1][4], 'o', label="iteration " + str(i + 1), linewidth=3.0)
+            # ac6.plot(iterations[i], his_weights[i][1][5]-his_weights[i-1][1][5], 'o', label="iteration " + str(i + 1), linewidth=3.0)
+            # ac7.plot(iterations[i], his_weights[i][1][6]-his_weights[i-1][1][6], 'o', label="iteration " + str(i + 1), linewidth=3.0)
 
     # Multiplication history of the gradients
     plt.figure("Multiplication history of the gradients", figsize=(10, 4))
