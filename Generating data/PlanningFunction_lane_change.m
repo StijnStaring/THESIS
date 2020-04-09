efaf711b4e9e035c5ce874883b4bbb7168efaf9f
StijnStaring <stijnstaring@hotmail.com>
2020-04-09 11:06:30 +0200

    09-04-11-06

--- Generating data/PlanningFunction_lane_change.m
+++ Generating data/PlanningFunction_lane_change.m
@@ -1,11 +1,14 @@
-function [throttle, delta, x_sol_prev, lam_prev] = PlanningFunction_lane_change(current_states,x_sol_prev,lam_prev)
+function [controls] = PlanningFunction_lane_change(current_states)
 
-    global Planner
-
+    global  width_road x_sol_prev lam_prev theta planner_lane_change
+    
     % Solving the optimization problem by calling the CasADi function,
-    % previously implemented and loaded as a global variable
-    sol = Planner(current_states,x_sol_prev,lam_prev);
-    [throttle, delta, x_sol_prev, lam_prev] = full(sol);
+    % previously implemented and loaded as a global variable   
+    [controls,x_sol_prev,lam_prev] = planner_lane_change(current_states,theta,width_road,x_sol_prev,lam_prev);
+    % controls = [throttle (-), delta (rad)]
+    disp('simulation completed!')
+
+    
 
 
     
--- Generating data/test_loading.m
+++ Generating data/test_loading.m
@@ -1,12 +1,11 @@
-% clc
-% clearvars
-% close all
-function [controls,x_sol_prev,lam_prev] = test_loading()
+clc
+clearvars
+close all
 
 import casadi.*
-% load('previous_solution');
-% x_sol_prev = previous_solution{1, 1}.x_sol_prev;
-% lam_prev = previous_solution{1, 1}.lam_prev;
+load('previous_solution');
+x_sol_prev = previous_solution{1, 1}.x_sol_prev;
+lam_prev = previous_solution{1, 1}.lam_prev;
 
 
 planner_lane_change = Function.load('planner_lane_change.casadi');
@@ -16,6 +15,4 @@ DM.set_precision(15);
 % outputs = [U[:,0],opti.x,opti.lam_g]
 [controls,x_sol_prev,lam_prev] = planner_lane_change([5,1,80/3.6,0.2,2*pi/180,1*pi/180],[4,5,6,1,2],3.46990715,x_sol_prev,lam_prev);
 disp('simulation completed!')
-
-end
 
